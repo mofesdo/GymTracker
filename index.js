a2eef0126b1669a113caf474b721dd006af4ca8b
Modesto Amador <mofesdo.dev@gmail.com>
2024-04-22 23:16:38 -0700

    making the table

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 console.log("connected")
+
+generateTable();
+function generateTable(){
+    let table = document.querySelector("#tableBody");
+    console.log(table)
+    for(let i = 0; i < table.children.length; i++){
+        let row = table.children[i];
+        for(let i = 0; i < 52; i++){
+            let box = document.createElement("td");
+            box.innerHTML = ""
+            row.appendChild(box)
+        }
+        console.log(row)
+
+    }
+
+}
